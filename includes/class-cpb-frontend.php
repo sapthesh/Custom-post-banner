64954967363ec69c036467966ee7abf132e45dbd
Sapthesh V <sapthesh@users.noreply.github.com>
2026-04-14 11:38:20 +0530

    Create class-cpb-frontend.php

---
+++ includes/class-cpb-frontend.php
@@ -0,0 +1,50 @@
+<?php
+
+if ( ! defined( 'ABSPATH' ) ) {
+    exit;
+}
+
+class CPB_Frontend {
+
+    public function __construct() {
+        add_action( 'wp_enqueue_scripts', array( $this, 'enqueue_assets' ) );
+        add_filter( 'the_content', array( $this, 'display_banner' ) );
+    }
+
+    public function enqueue_assets() {
+        if ( is_single() && get_option( 'cpb_banner_is_active', 0 ) ) {
+            wp_enqueue_style( 'cpb-style', CPB_PLUGIN_URL . 'assets/css/cpb-style.css', array(), '1.0.0' );
+            wp_enqueue_script( 'cpb-script', CPB_PLUGIN_URL . 'assets/js/cpb-script.js', array(), '1.0.0', true );
+
+            // Inject dynamic color options into the CSS
+            $bg_color   = get_option( 'cpb_banner_bg_color', '#ffffe0' );
+            $text_color = get_option( 'cpb_banner_text_color', '#333333' );
+            
+            $custom_css = "
+                .cpb-banner-wrapper {
+                    background-color: {$bg_color};
+                    color: {$text_color};
+                }
+            ";
+            wp_add_inline_style( 'cpb-style', $custom_css );
+        }
+    }
+
+    public function display_banner( $content ) {
+        if ( is_single() && is_main_query() && get_option( 'cpb_banner_is_active', 0 ) ) {
+            $banner_text = get_option( 'cpb_banner_content', '' );
+            
+            if ( empty( $banner_text ) ) {
+                return $content;
+            }
+
+            ob_start();
+            include CPB_PLUGIN_DIR . 'templates/banner-view.php';
+            $banner_html = ob_get_clean();
+
+            return $banner_html . $content;
+        }
+
+        return $content;
+    }
+}
